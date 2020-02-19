07752b5cc999323af6d36232497ccd505829f5df
AlexandrDeb <alexanderpgs@ukr.net>
2020-02-20 00:53:27 +0200

    [lev-1] firsts components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://png.pngtree.com/png-clipart/20190515/original/pngtree-coffee-time-png-image_3626459.jpg"
+             alt=""/>
+    </header>
+};
+
+export default Header;
